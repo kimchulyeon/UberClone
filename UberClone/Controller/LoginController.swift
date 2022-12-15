d6c42d41910069f3c2499a4b5c17f743cfcf79b6
chulyeon <guinness987@gmail.com>
2022-12-15 13:46:43 +0900

    Login viewController

---
+++ UberClone/Controller/LoginController.swift
@@ -0,0 +1,30 @@
+import UIKit
+
+class LoginController: UIViewController {
+
+	//MARK: - Properties
+	private let titleLabel: UILabel = {
+		let label = UILabel()
+		label.text = "UBER"
+		label.font = UIFont(name: "Avenir-Light", size: 36)
+		label.textColor = UIColor(white: 1, alpha: 0.8)
+		return label
+	}()
+	
+	//MARK: - LifeCycle
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		
+		self.view.backgroundColor = UIColor.init(red: 25/255, green: 25/255, blue: 25/255, alpha: 1)
+		
+		self.view.addSubview(titleLabel)
+		
+		titleLabel.anchor(top: self.view.safeAreaLayoutGuide.topAnchor)
+	}
+	
+	// 상태바 색상
+	override var preferredStatusBarStyle: UIStatusBarStyle {
+		return .lightContent
+	}
+}
