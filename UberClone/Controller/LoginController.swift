9b172ec5e19ba1e758c75cfee8bcaf141c4b8d70
chulyeon <guinness987@gmail.com>
2022-12-15 15:56:01 +0900

    view + view 를 Stack View로 변경

--- UberClone/Controller/LoginController.swift
+++ UberClone/Controller/LoginController.swift
@@ -13,7 +13,9 @@ class LoginController: UIViewController {
 	
 	// lazy var로 해줘야 밖에 View에 접근 가능하다.
 	private lazy var emailContainerView: UIView = {
-		return UIView().inputContainerView(img: UIImage(systemName: "envelope")!, txtField: emailTextField)
+		let view = UIView().inputContainerView(img: UIImage(systemName: "envelope")!, txtField: emailTextField)
+		view.heightAnchor.constraint(equalToConstant: 50).isActive = true // 스택뷰를 사용하면 그 안의 뷰 하나는 높이값을 가지고 있어야한다.
+		return view
 	}()
 	
 	private lazy var passwordContainerView: UIView = {
@@ -40,11 +42,21 @@ class LoginController: UIViewController {
 		titleLabel.anchor(top: self.view.safeAreaLayoutGuide.topAnchor)
 		titleLabel.centerX(inView: self.view)
 		
-		self.view.addSubview(emailContainerView)
-		emailContainerView.anchor(top: titleLabel.bottomAnchor, left: self.view.leadingAnchor, right: self.view.trailingAnchor, paddingTop: 40, paddingLeft: 16, paddingRight: 16, height: 50 )
+//		self.view.addSubview(emailContainerView)
+//		emailContainerView.anchor(top: titleLabel.bottomAnchor, left: self.view.leadingAnchor, right: self.view.trailingAnchor, paddingTop: 40, paddingLeft: 16, paddingRight: 16, height: 50 )
+//
+//		self.view.addSubview(passwordContainerView)
+//		passwordContainerView.anchor(top: emailContainerView.bottomAnchor, left: self.view.leadingAnchor, right: self.view.trailingAnchor, paddingTop: 16, paddingLeft: 16, paddingRight: 16, height: 50 )
 		
-		self.view.addSubview(passwordContainerView)
-		passwordContainerView.anchor(top: emailContainerView.bottomAnchor, left: self.view.leadingAnchor, right: self.view.trailingAnchor, paddingTop: 16, paddingLeft: 16, paddingRight: 16, height: 50 )
+		let stack = UIStackView(arrangedSubviews: [emailContainerView, passwordContainerView])
+		stack.axis = .vertical
+		stack.distribution = .fillEqually
+		stack.spacing = 16
+		
+		self.view.addSubview(stack)
+		stack.anchor(top: titleLabel.bottomAnchor, left: self.view.leadingAnchor, right: self.view.trailingAnchor, paddingTop: 40, paddingLeft: 16, paddingRight: 16)
+												   
+		
 	}
 	
 	// 상태바 색상
