3bc610b9899b869b149bdaa417bfffa373a41a0e
chulyeon <guinness987@gmail.com>
2022-12-15 13:30:56 +0900

    반복적인 constraint 과정 함수로 선언

---
+++ UberClone/Utils/Extensions.swift
@@ -0,0 +1,50 @@
+import UIKit
+
+/**
+	- UIView > UILabel : titleLabel.anchor()가 가능
+	- width와 height는 Label같은 경우 값을 주지 않으면 알아서 크기를 지정해주기 때문에 nil을 default값으로 한다.
+	- right | bottom은 음수로 작성해줘야한다.
+ */
+extension UIView {
+
+	/// constraint 반복 작업 함수
+	func anchor(top: NSLayoutYAxisAnchor? = nil,
+	            left: NSLayoutXAxisAnchor? = nil,
+	            bottom: NSLayoutYAxisAnchor? = nil,
+	            right: NSLayoutXAxisAnchor? = nil,
+	            paddingTop: CGFloat = 0,
+	            paddingLeft: CGFloat = 0,
+	            paddingBottom: CGFloat = 0,
+	            paddingRight: CGFloat = 0,
+	            width: CGFloat? = nil,
+	            height: CGFloat? = nil) {
+		
+		self.translatesAutoresizingMaskIntoConstraints = false
+		
+		if let top = top {
+			self.topAnchor.constraint(equalTo: top, constant: paddingTop).isActive = true
+		}
+		
+		if let left = left {
+			self.leadingAnchor.constraint(equalTo: left, constant: paddingLeft).isActive = true
+		}
+		
+		if let right = right {
+			self.trailingAnchor.constraint(equalTo: right, constant: -paddingRight).isActive = true
+		}
+		
+		if let bottom = bottom {
+			self.bottomAnchor.constraint(equalTo: bottom, constant: -paddingBottom).isActive = true
+		}
+		
+		if let width = width {
+			self.widthAnchor.constraint(equalToConstant: width).isActive = true
+		}
+		
+		if let height = height {
+			self.heightAnchor.constraint(equalToConstant: height).isActive = true
+		}
+		
+	}
+}
+
